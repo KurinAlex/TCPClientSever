162271b9c68daacb5877e4f3ed422f257d37b613
KurinAlex <olexkurin@gmail.com>
2023-10-21 01:38:59 +0300

    Server and Client implemented
    Implemented logic for Client connection and Server accepting the Client

---
+++ Utility/Data.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace Utility
+{
+    public static class Data
+    {
+        public const short Port = 6116;
+        public const string Host = "127.0.0.1";
+        public static readonly IPAddress HostIP = IPAddress.Parse(Host);
+        public static readonly IPEndPoint IPEndPoint = new(HostIP, Port);
+    }
+}
